0d978011b76ee55ad4b08e1e46b894445d67e3ab
Darshan Gada <gadadarshan@gmail.com>
2020-10-02 17:57:14 +0530

    Fix Redirect path

--- components/auth.js
+++ components/auth.js
@@ -11,61 +11,61 @@ import { mapUserData } from '../utils/auth/mapUserData'
 initFirebase()
 
 const firebaseAuthConfig = {
-  signInFlow: 'popup',
-  // Auth providers
-  // https://github.com/firebase/firebaseui-web#configure-oauth-providers
-  signInOptions: [
-    {
-      // Google provider must be enabled in Firebase Console to support one-tap
-      // sign-up.
-      provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      // Required to enable ID token credentials for this provider.
-      // This can be obtained from the Credentials page of the Google APIs
-      // console. Use the same OAuth client ID used for the Google provider
-      // configured with GCIP or Firebase Auth.
-      // clientId: 'xxxxxxxxxxxxxxxxx.apps.googleusercontent.com'
+    signInFlow: 'popup',
+    // Auth providers
+    // https://github.com/firebase/firebaseui-web#configure-oauth-providers
+    signInOptions: [
+        {
+            // Google provider must be enabled in Firebase Console to support one-tap
+            // sign-up.
+            provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+            // Required to enable ID token credentials for this provider.
+            // This can be obtained from the Credentials page of the Google APIs
+            // console. Use the same OAuth client ID used for the Google provider
+            // configured with GCIP or Firebase Auth.
+            // clientId: 'xxxxxxxxxxxxxxxxx.apps.googleusercontent.com'
+        },
+        {
+            provider: firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+            recaptchaParameters: {
+                type: 'image', // 'audio'
+                size: 'invisible', // 'invisible' or 'compact'
+                badge: 'bottomright' //' bottomright' or 'inline' applies to invisible.
+            },
+            defaultCountry: 'IN',
+            loginHint: '+919029208698',
+            whitelistedCountries: ['IN']
+        },
+    ],
+    signInSuccessUrl: process.env.basePath + '/',
+    credentialHelper: 'none',
+    callbacks: {
+        signInSuccessWithAuthResult: async ({ user }, redirectUrl) => {
+            const userData = mapUserData(user)
+            setUserCookie(userData)
+        },
     },
-    {
-      provider: firebase.auth.PhoneAuthProvider.PROVIDER_ID,
-      recaptchaParameters: {
-        type: 'image', // 'audio'
-        size: 'invisible', // 'invisible' or 'compact'
-        badge: 'bottomright' //' bottomright' or 'inline' applies to invisible.
-      },
-      defaultCountry: 'IN',
-      loginHint: '+919029208698',
-      whitelistedCountries: ['IN']
-    },
-  ],
-  signInSuccessUrl: '/',
-  credentialHelper: 'none',
-  callbacks: {
-    signInSuccessWithAuthResult: async ({ user }, redirectUrl) => {
-      const userData = mapUserData(user)
-      setUserCookie(userData)
-    },
-  },
 }
 
 const FirebaseAuth = () => {
-  // Do not SSR FirebaseUI, because it is not supported.
-  // https://github.com/firebase/firebaseui-web/issues/213
-  const [renderAuth, setRenderAuth] = useState(false)
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      setRenderAuth(true)
-    }
-  }, [])
-  return (
-    <div>
-      {renderAuth ? (
-        <StyledFirebaseAuth
-          uiConfig={firebaseAuthConfig}
-          firebaseAuth={firebase.auth()}
-        />
-      ) : null}
-    </div>
-  )
+    // Do not SSR FirebaseUI, because it is not supported.
+    // https://github.com/firebase/firebaseui-web/issues/213
+    const [renderAuth, setRenderAuth] = useState(false)
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            setRenderAuth(true)
+        }
+    }, [])
+    return (
+        <div>
+            {renderAuth ? (
+                <StyledFirebaseAuth
+                    uiConfig={firebaseAuthConfig}
+                    firebaseAuth={firebase.auth()}
+                />
+            ) : null}
+        </div>
+    )
 }
 
 export default FirebaseAuth
